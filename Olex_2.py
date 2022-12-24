fe18b3bfbacf3c8ca54f04fae6afc82d11848889
Emre Tolga Kaptan <112636626+Tikkaaa3@users.noreply.github.com>
2022-12-24 16:48:09 +0100

    Update Olex_2.py

--- Olex_2.py
+++ Olex_2.py
@@ -1,4 +1,3 @@
-#1- renkleri fontları düzenle, icon bul 2-text dosyası ve fotoğrafları uygulamanın içine göm 3-tanım veya kelime uzun olunca pencerenin dışına taşıyor(just Lexicon için geçerli) 4- lexicondaki kelimeler aşağıda kalıyor güncelleme yapılınca scrollbar değişimi algılamıyor.  6- app az kelimeyle açılırsa lexicondaki scrollbar çalışmıyor. 7- enter tuşuna entrylerin fonksiyonlarını atadım, butonlar çalışmıyor şimdide.(aynı fonksiyonu iki kere yazıp birinin içine event yazmayıp onu butona atarsak çözülüyor ama daha hızlı bir çözüm var mıdır ki?) 8- search kısmında guess sistemi olmalı 9- örnek cümle eklemesi güzel olur.
 import random
 from random import choice
 from cgitb import text
